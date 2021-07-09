aa58c435620a789283c78971245ad09fb6c497e7
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-07-09 15:54:20 +1000

    Update database.py

--- database.py
+++ database.py
@@ -52,23 +52,5 @@ class BaseModel(pw.Model):
 		database = db
 
 
-class KarmaUser(BaseModel):
+class User(BaseModel):
 	userid = pw.IntegerField(null=False)
-	chatid = pw.IntegerField(null=False)
-	karma = pw.IntegerField(null=False)
-	user_name = pw.CharField(max_length=100, null=False)
-	user_nick = pw.CharField(max_length=50, null=False)
-	is_freezed = pw.BooleanField(column_name="is_banned")
-
-	class Meta:
-		db_table = "karma_user"
-		primary_key = pw.CompositeKey("userid", "chatid")
-
-
-class Limitation(BaseModel):
-	userid = pw.IntegerField(null=False)
-	chatid = pw.IntegerField(null=False)
-	timer = pw.TimestampField(null=False, primary_key=True)
-
-	class Meta:
-		db_table = "limitation"
