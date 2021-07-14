9f8f371c68a36c89fcafa7d2e94e3005d160e669
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-07-14 12:09:44 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -17,15 +17,17 @@ bot = telebot.TeleBot(TELEGRAM_API)
 
 @bot.message_handler(commands=["start"])
 def start(msg):
-	bot.send_message(msg.chat.id, "Делитесь новостями, присылайте фото знакомьтесь и общайтесь, а наш Бот в этом вам поможет!")
+
 	
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	chanel = types.KeyboardButton(text="🔈 Каналы")
 	chats = types.KeyboardButton(text="💬 Чаты")
 	bots = types.KeyboardButton(text="🔘 Боты")
-	addcat = types.KeyboardButton(text="Добавить в каталог")
-	keyboard.add(chanel, chats, bots, addcat)
-	bot.send_message(msg.chat.id, "Хабаровские каналы, чаты и боты. Выберите рубрику на кнопках ниже ⤵️", reply_markup=keyboard)
+	addcat = types.KeyboardButton(text="☑️ Добавить в каталог")
+	addnews = types.KeyboardButton(text="☑️ Прислать новость")
+	addlove = types.KeyboardButton(text="☑️ Знакомства")
+	keyboard.add(chanel, chats,bots,addnews,addcat,addlove)
+	bot.send_message(msg.chat.id, "Делитесь новостями, присылайте фото знакомьтесь и общайтесь, а наш Бот в этом вам поможет! Хабаровские каналы, чаты и боты. Выберите рубрику на кнопках ниже ⤵️", reply_markup=keyboard)
     
 	selected_user = Users.select().where(
 		Users.userid == msg.from_user.id)
