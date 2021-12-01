3ff3dc693660d58f404e40617d428870e09599cc
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-12-01 13:07:09 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -182,9 +182,8 @@ def all_messages(msg):
 			bot.send_message(TO_CHAT_ID, "отправлено")
 	else:
 		bot.forward_message(TO_CHAT_ID, msg.chat.id, msg.message_id)
+		bot.send_message(TO_CHAT_ID, f"От: {msg.from_user.first_name} id: {msg.from_user.id}")
 		
-		bot.send_message(TO_CHAT_ID, f"От: <a href='tg://user?id={msg.from_user.id}'>{msg.from_user.first_name}</a", parse_mode="HTML")
-
 		bot.send_message(msg.chat.id, f"{msg.from_user.first_name} ваше сообщение получено.")
 		main(msg)
 
