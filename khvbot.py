f01342070cbd04826d0bffba89dbf04cea4ac8b7
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-12-01 12:48:03 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -129,7 +129,6 @@ def longname(call):
 
 	if call.data == "delete":
 		bot.send_message(call.message.chat.id, f"<a href='tg://user?id=55910350'>💰</a> Удалить анкету в знакомствах 30р. Счет для <b>{call.from_user.first_name}</b>:\n<a href='https://qiwi.com/payment/form/99999?amount=30&extra[%27accountType%27]=nickname&extra[%27account%27]=JCRUSH&extra[%27comment%27]=Love_Khv{call.from_user.id}&blocked[2]=comment&blocked[1]=account'>💳 Оплатить</a> (ID {call.from_user.id})", parse_mode="HTML")
-		bot.send_message(-542531596, f"Удалить в знакомствах От: <a href='tg://user?id={call.from_user.id}'>{call.from_user.first_name}</a", parse_mode="HTML")
 
 @bot.message_handler(commands=["stat"])
 def stat(msg):
@@ -183,7 +182,7 @@ def all_messages(msg):
 			bot.send_message(TO_CHAT_ID, "отправлено")
 	else:
 		bot.forward_message(TO_CHAT_ID, msg.chat.id, msg.message_id)
-		
+		bot.send_message(TO_CHAT_ID, f"От: <a href='tg://user?id={msg.from_user.id}'>{msg.from_user.first_name}</a", parse_mode="HTML")
 		bot.send_message(msg.chat.id, f"{msg.from_user.first_name} ваше сообщение получено.")
 		main(msg)
 
