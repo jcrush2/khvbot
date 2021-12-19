c1eedd01bf8278dc1d7fd8ba5ad87b4e9344312e
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-12-19 16:14:14 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -207,21 +207,7 @@ def all_messages(msg):
 		main(msg)
 		
 	
-def exoooy(text,intro):
-	headers = {
-    'Content-Type': 'application/json',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 '
-                  '(KHTML, like Gecko) Version/14.1.1 Safari/605.1.15',
-    'Origin': 'https://yandex.ru',
-    'Referer': 'https://yandex.ru/',}
 
-	API_URL = 'https://yandex.ru/lab/api/yalm/text3'
-	payload = {"query":text, "intro":intro, "filter":1}
-	params = json.dumps(payload).encode('utf-8')
-	req = urllib.request.Request(API_URL, data=params, headers=headers)
-	response = urllib.request.urlopen(req)
-	ya=json.loads(response.read().decode('utf-8'))
-	return ya["text"]
 
 # bot.polling(none_stop=True)
 
