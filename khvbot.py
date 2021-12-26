88145ad0f8b82443c127015c34c951805704cd18
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-12-26 15:00:04 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -261,7 +261,7 @@ def channel_post(msg):
 	chat_id=msg.chat.id,
 	message_id=msg.message_id,
 	text=msg.text,
-	reply_markup=ZaBan_bottom(msg.sender_chat.first_name, msg.sender_chat.first_name),
+	reply_markup=ZaBan_bottom(msg.forward_from.user.first_name, msg.forward_from.user.first_name),
 	parse_mode='HTML')
 	
 def ZaBan_bottom(nameid, idname):
