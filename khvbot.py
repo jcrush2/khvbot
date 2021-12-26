ed24d884ae1877731de8768668e08dd39abb31c4
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-12-26 16:00:35 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -242,10 +242,10 @@ def all_messages(msg):
 	if msg.chat.id == TO_CHAT_ID:
 		if msg.text.lower() == "/вопрос":
 			bot.send_message(-1001310162579,f'⁉️ {msg.reply_to_message.text}', parse_mode="HTML")
-			bot.reply_to(msg.reply_to_message,f"⁉️ Вопрос отправлен <a href='https://t.me/khvtrip'>Знатокам Хабаровска</a>", parse_mode="HTML")
+
 		if msg.text.lower() == "/l":
-			bot.send_message(-1001446448774,f"От <b>{msg.from_user.first_name}</b>\n{msg.reply_to_message.text}\n\n<a href='tg://user?id={msg.from_user.id}'>Написать</a>", parse_mode="HTML")
-			bot.send_message(461300905,f"⁉️ знакомства отправлен <a href='https://t.me/khvtrip'>Знатокам Хабаровска</a>", parse_mode="HTML")
+			bot.send_message(-1001446448774,f"👤 от <b>{msg.from_user.first_name}</b>\n{msg.reply_to_message.text}\n\n<a href='tg://user?id={msg.from_user.id}'>📝 Написать</a>", parse_mode="HTML")
+
 			
 		else:
 			bot.copy_message(message_id=msg.message_id,chat_id=msg.reply_to_message.forward_from.id,from_chat_id=msg.chat.id)
