5f6de47ac908ed4ba39c4a084d583a320acac764
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-12-26 20:05:47 +1000

    Update khvbot.py

--- khvbot.py
+++ khvbot.py
@@ -207,11 +207,11 @@ def love_foto(msg):
 	bot.forward_message(-542531596, msg.chat.id, msg.message_id)
 	bot.send_message(-542531596, f"От: <a href='tg://user?id={msg.from_user.id}'>{msg.from_user.first_name}</a> id: {msg.from_user.id}", parse_mode="HTML")
 	if msg.document:
-		sent = bot.send_message(msg.chat.id, text="⚠️ Ошибка! Фото должно быть отправленно через галерею, повторите ⬇")
+		sent = bot.send_message(msg.chat.id, text="⚠️ Ошибка! Фото должно быть отправленно через галерею, повторите ⬇ <a href='/отмена'>/отмена</a>" , parse_mode="HTML")
 		bot.register_next_step_handler(sent, love_foto)
 		return
 	if msg.caption ==None:
-		sent = bot.send_message(msg.chat.id, text="⚠️ Ошибка! Пришлите свое фото и добавьте в подпись инфу о себе, контакты ⬇")
+		sent = bot.send_message(msg.chat.id, text="⚠️ Ошибка! Пришлите свое фото и добавьте в подпись инфу о себе, контакты ⬇ <a href='/отмена'>/отмена</a>" , parse_mode="HTML")
 		bot.register_next_step_handler(sent, love_foto)
 		return
 		
